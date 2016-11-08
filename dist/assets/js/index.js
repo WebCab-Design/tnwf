09e79a9ff465d9df6b7add1f7baf9829d4abfecc
Alexnader Elias <alex.steven.elias@gmail.com>
2016-11-07 21:12:42 -0700

    added form edits

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,9 +1,36 @@
 (function() {
-	'use strict';
+'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+
+	var form = document.querySelector('.form');
+
+	var guestList = form.querySelector('.guest-list');
+	var guestFirst = form.querySelector('.guest-first');
+	var guestLast = form.querySelector('.guest-last');
+	var guestHidden = form.querySelector('.guest-hidden');
+	var guestAdd = form.querySelector('.guest-add');
+	var guestTotalAmount = form.querySelector('.total-amount');
+
+	var total = 0;
+
+	guestAdd.addEventListener('click', function () {
+		var li = document.createElement('li');
+		var guest = guestFirst.value + ' ' + guestLast.value;
+
+		li.innerText = guest;
+		guestFirst.value = '';
+		guestLast.value = '';
+		guestHidden.value = guestHidden.value + ', ' + guest;
+		guestList.appendChild(li);
+
+		total++;
+
+		guestTotalAmount.innerText = total;
+	});
 
 	At.submit({
+		query: form,
 		mimeType: 'json',
-		query: '.form',
 		complete: function (error, success) {
 			var response = document.querySelector('.response');
 
@@ -19,4 +46,5 @@
 		}
 	});
 
+});
 }());
