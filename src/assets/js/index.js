145bf0f69a3a22f5d55a05488829f40c109a0d6d
Alexander Elias <alex.steven.elias@gmail.com>
2016-11-23 15:10:42 -0700

    added form updates

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,73 +1,28 @@
-(function() {
-'use strict';
-
-// function offlineButtonRender () {
-// 	var offlineButton = document.querySelector('.offline-button');
-// 	var paymentWidget = document.querySelector('.payment-widget');
-// 	offlineButton.addEventListener('click', function () {
-// 		paymentWidget.style.display = 'none';
-// 	});
-// }
-//
-// function onlineButtonRender (total) {
-// 	var paymentWidget = document.querySelector('.payment-widget');
-// 	paypal.Button.render({
-// 		env: 'sandbox', // production, sandbox
-// 		commit: true,
-// 		client: {
-// 			sandbox: 'AYYG8wFherEY7ZmyS1biclrOmhxsNKxdNoyOwk85_sKve83LNTeHjAk2GP4Lxu0nqS5n1v4IZn2CFfZs'
-// 		},
-// 		style: {
-// 			shape: 'rect'
-// 		},
-// 		payment: function() {
-// 			var env    = this.props.env;
-// 			var client = this.props.client;
-//
-// 			return paypal.rest.payment.create(env, client, {
-// 				transactions: [{
-// 					amount: { total: total, currency: 'USD' }
-// 				}]
-// 			});
-// 		},
-// 		onAuthorize: function(data, actions) {
-// 			return actions.payment.execute().then(function() {
-// 				paymentWidget.style.display = 'none';
-// 			});
-// 		},
-// 		onCancel: function(data, actions) {
-// 			paymentWidget.style.display = 'none';
-// 		},
-// 		onError: function(error) {
-// 			console.log(error);
-// 		}
-// 	}, '.paypal-button');
-// }
-
 document.addEventListener('DOMContentLoaded', function () {
+	var amount = document.querySelector('.payment-widget input[name=amount]');
 	var paymentWidget = document.querySelector('.payment-widget');
 	var showFormButton = document.querySelector('.show-form');
+	var ticket = document.querySelector('.ticket');
 	var form = document.querySelector('.form');
 	var total = 0;
 
 	if (showFormButton) {
-		var form = document.querySelector('.form');
 		showFormButton.addEventListener('click', function () {
 			form.style.display = 'block';
 		});
 	}
 
-	if (form) {
-		if (document.querySelector('.ticket')) {
-			var ticketList = form.querySelector('.ticket-list');
-			var ticketFirst = form.querySelector('.ticket-first');
-			var ticketLast = form.querySelector('.ticket-last');
-			var ticketHidden = form.querySelector('.ticket-hidden');
-			var ticketAdd = form.querySelector('.ticket-add');
-			var ticketTotal = form.querySelector('.ticket-total');
-			var ticketCost = Number(ticketAdd.getAttribute('data-cost'));
+	if (ticket) {
+		var ticketList = form.querySelector('.ticket-list');
+		var ticketFirst = form.querySelector('.ticket-first');
+		var ticketLast = form.querySelector('.ticket-last');
+		var ticketHidden = form.querySelector('.ticket-hidden');
+		var ticketAdd = form.querySelector('.ticket-add');
+		var ticketTotal = form.querySelector('.ticket-total');
+		var ticketCost = Number(ticketAdd.getAttribute('data-cost'));
 
-			ticketAdd.addEventListener('click', function () {
+		ticketAdd.addEventListener('click', function () {
+			if (ticketFirst.value.length !== 0 && ticketLast.value.length !== 0) {
 				var li = document.createElement('li');
 				var ticket = ticketFirst.value + ' ' + ticketLast.value;
 
@@ -79,28 +34,40 @@ document.addEventListener('DOMContentLoaded', function () {
 				else ticketHidden.value = ticketHidden.value + ', ' + ticket;
 
 				ticketList.appendChild(li);
-				total = total + ticketCost;
-				ticketTotal.value = total;
-			});
-		}
+				total = Number(total) + ticketCost;
+				ticketTotal.value = total.toString();
+			} else {
+				window.alert('First Name and Last Name are required');
+			}
+		});
+	}
 
-		var paypalSubmit = document.querySelector('.online-button > input[type="submit"]');
-		if (paypalSubmit) {
-			paypalSubmit.addEventListener('click', function () {
-				this.parentNode.parentNode.parentNode.style.display = 'none';
-			});
-		}
-
+	if (form) {
 		Astatine.submit({
 			query: form,
 			mimeType: 'json',
-			// prepare: function (data) {
-				// if (paymentWidget) {
-				// 	offlineButtonRender();
-				// 	onlineButtonRender(data.Total.toString());
-				// 	paymentWidget.style.display = 'block';
-				// }
-			// },
+			prepare: function (data) {
+
+				if (paymentWidget) {
+					var onlineButton = document.querySelector('.online-button');
+					var offlineButton = document.querySelector('.offline-button');
+					var formName = document.querySelector('input[name="Form Name"]');
+					var itemName = document.querySelector('input[name="item_name"]');
+					var handlePayment = function () { paymentWidget.style.display = 'none'; };
+
+					if (total == 0) {
+						window.alert('Requires at least one individual.');
+						throw new Error('Requires at least one individual.');
+					} else {
+						itemName.value = formName.value;
+						amount.value = total.toString();
+						paymentWidget.style.display = 'block';
+						onlineButton.addEventListener('click', handlePayment);
+						offlineButton.addEventListener('click', handlePayment);
+					}
+				}
+
+			},
 			complete: function (error, success) {
 				var response = document.querySelector('.response');
 
@@ -119,4 +86,3 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 });
-}());
