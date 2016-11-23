73fd3b4f020bbbe5417480448e7ab4b3bc14b68b
Jon Burns <jonburns10@gmail.com>
2016-11-23 08:25:22 -0700

    Docs pack

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,8 +1,55 @@
 (function() {
 'use strict';
-document.addEventListener('DOMContentLoaded', function () {
 
+// function offlineButtonRender () {
+// 	var offlineButton = document.querySelector('.offline-button');
+// 	var paymentWidget = document.querySelector('.payment-widget');
+// 	offlineButton.addEventListener('click', function () {
+// 		paymentWidget.style.display = 'none';
+// 	});
+// }
+//
+// function onlineButtonRender (total) {
+// 	var paymentWidget = document.querySelector('.payment-widget');
+// 	paypal.Button.render({
+// 		env: 'sandbox', // production, sandbox
+// 		commit: true,
+// 		client: {
+// 			sandbox: 'AYYG8wFherEY7ZmyS1biclrOmhxsNKxdNoyOwk85_sKve83LNTeHjAk2GP4Lxu0nqS5n1v4IZn2CFfZs'
+// 		},
+// 		style: {
+// 			shape: 'rect'
+// 		},
+// 		payment: function() {
+// 			var env    = this.props.env;
+// 			var client = this.props.client;
+//
+// 			return paypal.rest.payment.create(env, client, {
+// 				transactions: [{
+// 					amount: { total: total, currency: 'USD' }
+// 				}]
+// 			});
+// 		},
+// 		onAuthorize: function(data, actions) {
+// 			return actions.payment.execute().then(function() {
+// 				paymentWidget.style.display = 'none';
+// 			});
+// 		},
+// 		onCancel: function(data, actions) {
+// 			paymentWidget.style.display = 'none';
+// 		},
+// 		onError: function(error) {
+// 			console.log(error);
+// 		}
+// 	}, '.paypal-button');
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+	var paymentWidget = document.querySelector('.payment-widget');
 	var showFormButton = document.querySelector('.show-form');
+	var form = document.querySelector('.form');
+	var total = 0;
+
 	if (showFormButton) {
 		var form = document.querySelector('.form');
 		showFormButton.addEventListener('click', function () {
@@ -10,22 +57,15 @@ document.addEventListener('DOMContentLoaded', function () {
 		});
 	}
 
-	var form = document.querySelector('.form');
 	if (form) {
-
-		var ticket = form.querySelector('.ticket');
-
-		if (ticket) {
+		if (document.querySelector('.ticket')) {
 			var ticketList = form.querySelector('.ticket-list');
 			var ticketFirst = form.querySelector('.ticket-first');
 			var ticketLast = form.querySelector('.ticket-last');
 			var ticketHidden = form.querySelector('.ticket-hidden');
 			var ticketAdd = form.querySelector('.ticket-add');
 			var ticketTotal = form.querySelector('.ticket-total');
-
 			var ticketCost = Number(ticketAdd.getAttribute('data-cost'));
-
-			var total = 0;
 
 			ticketAdd.addEventListener('click', function () {
 				var li = document.createElement('li');
@@ -39,16 +79,28 @@ document.addEventListener('DOMContentLoaded', function () {
 				else ticketHidden.value = ticketHidden.value + ', ' + ticket;
 
 				ticketList.appendChild(li);
-
 				total = total + ticketCost;
-
 				ticketTotal.value = total;
+			});
+		}
+
+		var paypalSubmit = document.querySelector('.online-button > input[type="submit"]');
+		if (paypalSubmit) {
+			paypalSubmit.addEventListener('click', function () {
+				this.parentNode.parentNode.parentNode.style.display = 'none';
 			});
 		}
 
 		Astatine.submit({
 			query: form,
 			mimeType: 'json',
+			// prepare: function (data) {
+				// if (paymentWidget) {
+				// 	offlineButtonRender();
+				// 	onlineButtonRender(data.Total.toString());
+				// 	paymentWidget.style.display = 'block';
+				// }
+			// },
 			complete: function (error, success) {
 				var response = document.querySelector('.response');
 
@@ -58,7 +110,7 @@ document.addEventListener('DOMContentLoaded', function () {
 					response.innerText = 'Error';
 				} else {
 					form.style.display = 'none';
-					response.style.color = 'green';
+					response.style.color = '#6db4b1';
 					response.innerText = 'Form Is Submitted';
 				}
 
