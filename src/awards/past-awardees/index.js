5537d077fac4a17bc68f8a7b02315dc8eef78f89
Jon Burns <jonburns10@gmail.com>
2020-11-16 14:35:55 -0800

    2020 winners, clean up

--- src/awards/past-awardees/index.js
+++ src/awards/past-awardees/index.js
@@ -4,364 +4,364 @@ var awardees = [
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/ChristopherPaulApodaca.jpg'
     },
     {
         name: 'Julie Arbogast',
         facility: 'Tucson Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/JulieArbogast.jpg'
     },
     {
         name: 'Clare Armet',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/ClareArmet.jpg'
     },
     {
         name: 'Amy Baker',
         facility: 'Carondelet St. Mary\'s Hospital',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/AmyBaker.jpg'
     },
     {
         name: 'Kaylyn Bautista',
         facility: 'Optum Rx Infusion Services',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/KaylynBautista.jpg'
     },
     {
         name: 'Nicole Clark',
         facility: 'Carondelet St. Joseph\'s Hospital',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/NicoleClark.jpg'
     },
     {
         name: 'Patricia Clem',
         facility: 'Northwest Oro Valley Hospital',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/PatriciaClem.jpg'
     },
     {
         name: 'Miki Couchoud',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/MikiCouchoud.jpg'
     },
     {
         name: 'Jeri L. Crews',
         facility: 'Northwest Oro Valley Hospital',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/JeriLCrews.jpg'
     },
     {
         name: 'Andrew Czosek',
         facility: 'Tucson Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/AndrewCzosek.jpg'
     },
     {
         name: 'Jennifer de Boer',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/JenniferdeBoer.jpg'
     },
     {
         name: 'Teresa Dennis',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/TeresaDennis.jpg'
     },
     {
         name: 'Diane M. Deutsch',
         facility: 'Casa de la Luz',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/DianeMDeutsch.jpg'
     },
     {
         name: 'Peggy Eisele',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/PeggyEisele.jpg'
     },
     {
         name: 'Erica Engelbretson-Medford',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/EricaEngelbretson-Medford.jpg'
     },
     {
         name: 'Sandy Farrer',
         facility: 'Northwest Oro Valley Hospital',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/SandraFarrer.jpg'
     },
     {
         name: 'Vernold M. Fernandez',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/VernoldMFernandez.jpg'
     },
     {
         name: 'Dae Fernandez',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/DaeFernandez.jpg'
     },
     {
         name: 'Rowena B. Geraldoy',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/RowenaBGeraldoy.jpg'
     },
     {
         name: 'Amy Ginn',
         facility: 'Casa de la Luz',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/AmyGinn.jpg'
     },
     {
         name: 'Becky Heral',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/BeckyHeral.jpg'
     },
     {
         name: 'Sharon Hom',
         facility: 'University of Arizona College of Nursing',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/SharonHom.jpg'
     },
     {
         name: 'Leslie James',
         facility: 'Arizona Lifeline',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/LeslieJames.jpg'
     },
     {
         name: 'Jeanne Jones',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/JeanneJones.jpg'
     },
     {
         name: 'Ron Juarez',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/RonJuarez.jpg'
     },
     {
         name: 'Melissa Koon',
         facility: 'Casa de la Luz',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/MelissaKoon.jpg'
     },
     {
         name: 'Susan Macdonald',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/SusanMacdonald.jpg'
     },
     {
         name: 'Sharon McCaig',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/SharonMcCaig.jpg'
     },
     {
         name: 'Nancy McGuckin',
         facility: 'University of Arizona College of Nursing',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/NancyMcGuckin.jpg'
     },
     {
         name: 'Wendy Meade',
         facility: 'Northwest Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/WendyMeade.jpg'
     },
     {
         name: 'Linda Moe',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/LindaMoe.jpg'
     },
     {
         name: 'Trudy Morrison',
         facility: 'Casa de la Luz',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/TrudyMorrison.jpg'
     },
     {
         name: 'Aaron Murillo',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/AaronMurillo.jpg'
     },
     {
         name: 'Shawn Mulligan',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/ShawnMulligan.jpg'
     },
     {
         name: 'Isabel Muñoz-Tucker',
         facility: 'Tucson Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/IsabelMunoz-Tucker.jpg'
     },
-    {
-        name: 'Pamela G. Reed',
-        facility: 'University of Arizona College of Nursing',
-        year: 2020,
-        type: 'fab50',
-        img: ''
-    },
+    // {
+    //     name: 'Pamela G. Reed',
+    //     facility: 'University of Arizona College of Nursing',
+    //     year: 2020,
+    //     type: 'fab50',
+    //     img: '/assets/images/awardees2020/.jpg'
+    // },
     {
         name: 'Tammy Renae Rhein',
         facility: 'Northwest Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/TammyRenaeRhein.jpg'
     },
     {
         name: 'Tera Schuetta',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/TeraSchuetta.jpg'
     },
     {
         name: 'Alexander Serwe-Montoya',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/AlexandriaSerwe-Montoya.jpg'
     },
     {
         name: 'Kelly Smith',
         facility: 'Bayada Home Health Care',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/KellySmith.jpg'
     },
     {
         name: 'Christopher A. Stump',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/ChristopherAStump.jpg'
     },
     {
         name: 'Christina Suarez',
         facility: 'Carondelet St. Mary\'s Hospital',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/ChristinaSuarez.jpg'
     },
     {
         name: 'Nancy Swank',
         facility: 'Northwest Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/NancySwank.jpg'
     },
-    {
-        name: 'Nanette Tamaray',
-        facility: 'Banner University Medical Center Tucson',
-        year: 2020,
-        type: 'fab50',
-        img: ''
-    },
+    // {
+    //     name: 'Nanette Tamaray',
+    //     facility: 'Banner University Medical Center Tucson',
+    //     year: 2020,
+    //     type: 'fab50',
+    //     img: '/assets/images/awardees2020/.jpg'
+    // },
     {
         name: 'Mary Thierman',
         facility: 'Tucson Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/MaryThierman.jpg'
     },
     {
         name: 'David G Varela',
         facility: 'Tucson Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/DavidGVarela.jpg'
     },
     {
         name: 'Angie Vargas',
         facility: 'Northwest Oro Valley Hospital',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/AngieVargas.jpg'
     },
     {
         name: 'Annette Vince',
         facility: 'Northwest Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/AnnetteVince.jpg'
     },
     {
         name: 'David S. Wood',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'fab50',
-        img: ''
+        img: '/assets/images/awardees2020/DavidSWood.jpg'
     },
     {
         name: 'Tamra Cullumber',
         facility: 'Banner University Medical Center Tucson',
         year: 2020,
         type: 'mentor',
-        img: ''
+        img: '/assets/images/awardees2020/TamraCullumber.jpg'
     },
     {
         name: 'Sheri M.Carson',
         facility: 'University of Arizona College of Nursing',
         year: 2020,
         type: 'stuetze',
-        img: ''
+        img: '/assets/images/awardees2020/SheriMCarson.jpg'
     },
     {
         name: 'La Boyer',
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'top5',
-        img: ''
+        img: '/assets/images/awardees2020/LaBoyer.jpg'
 
     },
     {
@@ -369,7 +369,7 @@ var awardees = [
         facility: 'El Rio Health',
         year: 2020,
         type: 'top5',
-        img: ''
+        img: '/assets/images/awardees2020/SharriDeLong.jpg'
 
     },
     {
@@ -377,7 +377,7 @@ var awardees = [
         facility: 'Southern Arizona VA Medical Center',
         year: 2020,
         type: 'top5',
-        img: ''
+        img: '/assets/images/awardees2020/MicheleSerrano.jpg'
 
     },
     {
@@ -385,7 +385,7 @@ var awardees = [
         facility: 'Casa de la Luz',
         year: 2020,
         type: 'top5',
-        img: ''
+        img: '/assets/images/awardees2020/StacyValdez.jpg'
 
     },
     {
@@ -393,7 +393,36 @@ var awardees = [
         facility: 'Casa de la Luz',
         year: 2020,
         type: 'top5',
-        img: ''
+        img: '/assets/images/awardees2020/PriscillaWilkinson.jpg'
 
     }
 ];
+
+var containers = {
+    top5Container: document.querySelector('.top5'),
+    fab50Container: document.querySelector('.fab50'),
+    mentorContainer: document.querySelector('.mentor'),
+    stuetzeContainer: document.querySelector('.stuetze'),
+}
+
+for (var awardee of awardees) {
+    var container = containers[awardee.type + 'Container'];
+
+    if (!container) continue;
+    
+    var org = document.createElement('p');
+    var img = document.createElement('img');
+    var title = document.createElement('h4');
+    var column = document.createElement('div');
+
+    column.setAttribute('class', 'col-xs-4 col-sm-3');
+    img.setAttribute('src', awardee.img);
+    title.innerText = awardee.name;
+    org.innerText = awardee.facility;
+
+    column.appendChild(img);
+    column.appendChild(title);
+    column.appendChild(org);
+    
+    container.appendChild(column);
+}
